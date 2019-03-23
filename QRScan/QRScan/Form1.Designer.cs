accf7729df60ea00ebc89b2244b9551ca7bef21d
rosen1997 <rosen_lechev@abv.bg>
2019-03-23 17:51:37 +0200

    Сканиране на QR Code Signed-off-by: rosen1997 <rosen_lechev@abv.bg>

---
+++ QRScan/QRScan/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace QRScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CMB_cameras = new System.Windows.Forms.ComboBox();
+            this.BTN_start = new System.Windows.Forms.Button();
+            this.BTN_stop = new System.Windows.Forms.Button();
+            this.BTN_scan = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LBL_test = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CMB_cameras
+            // 
+            this.CMB_cameras.FormattingEnabled = true;
+            this.CMB_cameras.Location = new System.Drawing.Point(541, 30);
+            this.CMB_cameras.Name = "CMB_cameras";
+            this.CMB_cameras.Size = new System.Drawing.Size(121, 24);
+            this.CMB_cameras.TabIndex = 0;
+            // 
+            // BTN_start
+            // 
+            this.BTN_start.Location = new System.Drawing.Point(541, 70);
+            this.BTN_start.Name = "BTN_start";
+            this.BTN_start.Size = new System.Drawing.Size(121, 28);
+            this.BTN_start.TabIndex = 1;
+            this.BTN_start.Text = "Start camera";
+            this.BTN_start.UseVisualStyleBackColor = true;
+            this.BTN_start.Click += new System.EventHandler(this.BTN_start_Click);
+            // 
+            // BTN_stop
+            // 
+            this.BTN_stop.Location = new System.Drawing.Point(541, 99);
+            this.BTN_stop.Name = "BTN_stop";
+            this.BTN_stop.Size = new System.Drawing.Size(121, 28);
+            this.BTN_stop.TabIndex = 2;
+            this.BTN_stop.Text = "Stop camera";
+            this.BTN_stop.UseVisualStyleBackColor = true;
+            this.BTN_stop.Click += new System.EventHandler(this.BTN_stop_Click);
+            // 
+            // BTN_scan
+            // 
+            this.BTN_scan.Location = new System.Drawing.Point(541, 466);
+            this.BTN_scan.Name = "BTN_scan";
+            this.BTN_scan.Size = new System.Drawing.Size(121, 23);
+            this.BTN_scan.TabIndex = 3;
+            this.BTN_scan.Text = "Scan";
+            this.BTN_scan.UseVisualStyleBackColor = true;
+            this.BTN_scan.Click += new System.EventHandler(this.BTN_scan_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1163, 300);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LBL_test
+            // 
+            this.LBL_test.AutoSize = true;
+            this.LBL_test.Location = new System.Drawing.Point(35, 30);
+            this.LBL_test.Name = "LBL_test";
+            this.LBL_test.Size = new System.Drawing.Size(46, 17);
+            this.LBL_test.TabIndex = 5;
+            this.LBL_test.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 514);
+            this.Controls.Add(this.LBL_test);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BTN_scan);
+            this.Controls.Add(this.BTN_stop);
+            this.Controls.Add(this.BTN_start);
+            this.Controls.Add(this.CMB_cameras);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CMB_cameras;
+        private System.Windows.Forms.Button BTN_start;
+        private System.Windows.Forms.Button BTN_stop;
+        private System.Windows.Forms.Button BTN_scan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LBL_test;
+    }
+}
+
---
+++ QRScan/QRScan/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using QRCodeEncoderDecoderLibrary;
+
+namespace QRScan
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection CaptureDevice; // list of webcam
+        private VideoCaptureDevice FinalFrame;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);//constructor
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                CMB_cameras.Items.Add(Device.Name);
+            }
+
+            CMB_cameras.SelectedIndex = 0; // default
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs) // must be void so that it can be accessed everywhere.
+        // New Frame Event Args is an constructor of a class
+        {
+            Bitmap bitmap = eventArgs.Frame;
+            pictureBox1.Image = (Bitmap)bitmap.Clone();
+            // pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();// clone the bitmap
+        }
+
+        private void BTN_start_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[CMB_cameras.SelectedIndex].MonikerString);// specified web cam and its filter moniker string
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);// click button event is fired, 
+            FinalFrame.Start();
+        }
+
+        private void BTN_stop_Click(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning == true) FinalFrame.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BTN_stop_Click(sender, e);
+        }
+
+        private void BTN_scan_Click(object sender, EventArgs e)
+        {
+            QRDecoder QRCodeDecoder = new QRDecoder();
+            byte[][] DataByteArray = QRCodeDecoder.ImageDecoder((Bitmap)pictureBox1.Image);
+            try
+            {
+                string Result = QRCode.ByteArrayToStr(DataByteArray[0]);
+                LBL_test.Text = Result;
+            }
+            catch { MessageBox.Show("Could not detect QR code!"); }
+        }
+    }
+}
